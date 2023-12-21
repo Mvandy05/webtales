a160f73c8e599d0aaf0757c4fcc56fd184f8a047
Matt Vanderhoef <43138880+Mvandy05@users.noreply.github.com>
2023-12-21 11:04:50 -0500

    Remove scrollbars

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,5 +69,9 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'), 
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar-hide')
+  ],
 }
